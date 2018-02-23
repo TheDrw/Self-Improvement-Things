b69f161d3509e031fb1ee74211d4d66b2444bf8c
Andrew Esberto <drwmakesgames@gmail.com>
2018-02-22 22:23:32 -0800

    fixed errors that arose when compiling
    fixed errors when compiling with -std=c++11 in ubuntu

--- C++/ZerothAttempt/vector/dvector.cpp
+++ C++/ZerothAttempt/vector/dvector.cpp
@@ -3,7 +3,7 @@
 namespace drw
 {
 
-	template<typename T> dvector<T>::dvector() : dSize(0), dCapacity(0)
+	template<typename T> dvector<T>::dvector() : dSize(0), dCapacity(0), arr(nullptr)
 	{
 		arr = new T[dSize];
 	}// dvector()
@@ -11,35 +11,44 @@ namespace drw
 	template<typename T> dvector<T>::~dvector()
 	{
 		delete []arr;
-		arr = NULL;
+		arr = nullptr;
 	}// ~dvector
 	
 	
 	////////////////////////////////////////////////
-	/////////////		CAPACITY		////////////
+	/////////////	     CAPACITY       ////////////
 	////////////////////////////////////////////////
 	
 	// return size of container or number of elements in container.
-	int template <typename T> dvector<T>::size(){ return dSize; }	
+	template<typename T> int dvector<T>::size() const
+	{ 
+		return dSize; 
+	}	
 
 	//int template <typename T> dvector<T>::max_size(){}
 	
 	// resizes the container. 
 	// capacity grows in powers of 2. resizes when size == capacity/2
-	void template <typename T> dvector<T>::resize(int newSize)
+	template<typename T> void dvector<T>::resize(int newSize)
 	{
 		if(newSize > dCapacity) reserve(dCapacity * 2 + 1);
 		dSize = newSize;
 	}
 	
 	// returns capacity of container
-	int template<typename T> dvector<T>::capacity(){ return dCapacity; }
+	template<typename T> int dvector<T>::capacity() const 
+	{ 
+		return dCapacity; 
+	}
 
 	// if size is greater than 0, return false. else return true.
-	bool template<typename T> dvector<T>::empty(){ return dSize > 0 ? false : true; }
+	template<typename T> bool dvector<T>::empty() const
+	{ 
+		return dSize == 0 ? true : false; 
+	}
 	
 	// allocate more space in container if needed. increases dCapacity. kind of cheated at this part.
-	void template <typename T> dvector<T>::reserve(int newCapacity)
+	template <typename T> void dvector<T>::reserve(int newCapacity)
 	{	
 		if(newCapacity < dSize) return; // if reserves less than what's already available, skip
 	
@@ -48,31 +57,34 @@ namespace drw
 		for(int i = 0; i < dSize; ++i) arr[i] = oldArr[i];	// copy elements in newly sized arr
 		
 		dCapacity = newCapacity;
-		delete []oldArr
+		delete []oldArr;
 	}
 	
 	////////////////////////////////////////////////
-	/////////////	 ELEMENT ACCESS		////////////
+	/////////////	 ELEMENT ACCESS	    ////////////
 	////////////////////////////////////////////////
 	
-	T template<typename T> dvector<T>::&operator[](int index) { return arr[index] != NULL ? arr[index] : NULL; }
+	template<typename T> T &dvector<T>::operator[](int index) const 
+	{ 
+		return arr[index] != nullptr ? arr[index] : nullptr; 
+	}
 		
 	
 	////////////////////////////////////////////////
-	/////////////		MODIFIERS		////////////
+	/////////////	    MODIFIERS       ////////////
 	////////////////////////////////////////////////
 	
 	// adds element to container at the end. increments dSize.
-	void template<typename T> dvector<T>::push_back(const T)
+	template<typename T> void dvector<T>::push_back(const T element)
 	{
 		if(dSize + 1 > dCapacity) reserve(dCapacity * 2 + 1);
 		
 		++dSize;
-		arr[dSize] = T;	// put new element at end of arr 
+		arr[dSize] = element;	// put new element at end of arr 
 	}
 	
 	// deletes last element. decrements dSize.
-	void template<typename T> dvector<T>::pop_back()
+	template<typename T> void dvector<T>::pop_back()
 	{
 		arr[dSize] = 0;						// deletes last element by naking it zero
 		dSize = dSize == 0 ? 0 : dSize - 1;	// if 0, keep it 0, else decrement dSize.
