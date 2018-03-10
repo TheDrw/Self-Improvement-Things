2aa0bba4ab4e2aecaf52d60da9f74f783fe47266
Andrew Esberto <drwmakesgames@gmail.com>
2018-03-10 13:12:32 -0800

    added init list

--- C++/queue/queue_array.h
+++ C++/queue/queue_array.h
@@ -14,9 +14,10 @@
 #ifndef QUEUE_ARRAY
 #define QUEUE_ARRAY
 
-#include <memory>		// std::unique_ptr
-#include <cstddef>		// std::size_t
-#include <iostream>		// std::cout
+#include <memory>						// std::unique_ptr
+#include <cstddef>					// std::size_t
+#include <iostream>					// std::cout
+#include <initializer_list>	//std::initializer_list
 
 namespace drw
 { 
@@ -26,13 +27,14 @@ namespace drw
 	public:
 		explicit queue_array();
 		explicit queue_array(const T&);
+		queue_array(std::initializer_list<T>);
 		~queue_array();
 
 		// member functions
 		bool empty();
 		std::size_t size() const;
-		T& front() const;
-		T& back() const;
+		T &front() const;
+		T	&back() const;
 		void push(const T&);
 		// emplace
 		void pop();
@@ -48,9 +50,9 @@ namespace drw
 
 	private:
 		std::unique_ptr<T[]> dArray;	// gonna change the style on array a bit.
-		std::size_t dSize;				// the d is used for differentiation.
-		std::size_t dCapacity;			// it doesn't mean differentiation.
-		std::size_t dFront;				// although, it could if you wanted it to.
+		std::size_t dSize;						// the d is used for differentiation.
+		std::size_t dCapacity;				// it doesn't mean differentiation.
+		std::size_t dFront;						// although, it could if you wanted it to.
 		std::size_t dBack;
 	};// class - queue_array 
 }// namespace - drw
